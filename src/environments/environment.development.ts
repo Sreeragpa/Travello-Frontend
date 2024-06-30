dbc2f07b1744d88e9e0a77c256196e48ecbcd117
Sreerag PA <catchsreerag@gmail.com>
2024-06-30 15:46:03 +0530

    backend doamin updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     geoapifyKey:"177668e9d9044d7b839472dd492b20d3",
-    backendDomain: 'http://localhost:3000',
-    socketUrl: 'http://localhost:3000'
+     backendDomain: 'https://api.travello.srg.buzz',
+    socketUrl: 'https://api.travello.srg.buzz'
 };
