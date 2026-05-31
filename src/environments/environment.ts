68627209bb924538acb812e68def4a02748ccfa3
Sreerag PA <catchsreerag@gmail.com>
2026-06-01 00:26:51 +0530

    Update backend domain in environment configuration to new Render URL for improved API access.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     geoapifyKey:"177668e9d9044d7b839472dd492b20d3",
-    backendDomain: 'https://api.travello.srgweb.site',
-    socketUrl: 'https://api.travello.srgweb.site',
+    backendDomain: 'https://travello-backend-70qu.onrender.com',
+    socketUrl: 'https://travello-backend-70qu.onrender.com',
     googleClientID: '696084527477-09ptrr8mh7mmufpivk2k2aeih2m91cvk.apps.googleusercontent.com'
 };
