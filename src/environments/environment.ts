64326327b5f76bbb83ebc1b8be397cce6d815484
Sreerag PA <catchsreerag@gmail.com>
2026-08-15 09:58:57 +0530

    Update environment configuration to switch socket URL for consistent deployment across environments.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
     geoapifyKey:"177668e9d9044d7b839472dd492b20d3",
-    // backendDomain: 'https://travello-backend-70qu.onrender.com',
     backendDomain: 'https://travello-backend-omega.vercel.app',
-    socketUrl: 'https://travello-backend-70qu.onrender.com',
+    socketUrl: 'https://travello-backend-omega.vercel.app',
     googleClientID: '696084527477-09ptrr8mh7mmufpivk2k2aeih2m91cvk.apps.googleusercontent.com'
 };
